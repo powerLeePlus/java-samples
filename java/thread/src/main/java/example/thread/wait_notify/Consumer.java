16bf3fc9f4d4ea96e56b8eeef8549b7087e29e5c
liwenqiang <liwenqiang10085@duia.com>
2020-11-26 19:09:02 +0800

    wait notify

---
+++ java/thread/src/main/java/example/thread/wait_notify/Consumer.java
@@ -0,0 +1,22 @@
+package example.thread.wait_notify;
+
+/**
+ * @author lwq
+ * @date 2020/11/26 0026
+ */
+public class Consumer implements Runnable {
+	private QueueBuffer q;
+
+	public Consumer(QueueBuffer q) {
+		this.q = q;
+		new Thread(this, "Consumer").start();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			q.get();
+			System.out.println();
+		}
+	}
+}
---
+++ java/thread/src/main/java/example/thread/wait_notify/QueueBuffer.java
@@ -0,0 +1,50 @@
+package example.thread.wait_notify;
+
+/**
+ * @author lwq
+ * @date 2020/11/26 0026
+ */
+public class QueueBuffer {
+	//计数
+	int n;
+	// 生产者生产了，记为true。消费者消费了，记为false。消费者不消费库存，就不再生产（保持库存为1）
+	boolean valueSet = false;
+
+	/**
+	 * 消费者调
+	 */
+	synchronized int get() {
+		System.out.println("Prepare Got: " + n + "......");
+		if (!valueSet) {
+			try {
+				// 生产者还没生产（没调put()），不会把valueSet置为true。就让消费者阻塞，不再消费
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Got: " + n);
+		valueSet = false;
+		this.notify();
+		return n;
+	}
+
+	/**
+	 * 生产者调
+	 */
+	synchronized void put(int n) {
+		System.out.println("Prepare put: " + n + "......");
+		if (valueSet) {
+			try {
+				// 消费者没消费（没调get()），不会把valueSet置为false。就让生产者阻塞，不再生产
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		this.n = n;
+		valueSet = true;
+		System.out.println("Put: " + n);
+		notify();
+	}
+}
