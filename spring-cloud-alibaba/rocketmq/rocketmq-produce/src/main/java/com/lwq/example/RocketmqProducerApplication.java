5613d46a46db2c3e7a47272a693027e855007ab1
liwenqiang <liwenqiang10085@duia.com>
2021-06-09 19:06:14 +0800

    spring 异步

---
+++ spring-cloud-alibaba/rocketmq/rocketmq-produce/src/main/java/com/lwq/example/RocketmqProducerApplication.java
@@ -0,0 +1,16 @@
+package com.lwq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lwq
+ * @date 2021/6/9 0009
+ */
+@SpringBootApplication
+public class RocketmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqProducerApplication.class, args);
+	}
+}
