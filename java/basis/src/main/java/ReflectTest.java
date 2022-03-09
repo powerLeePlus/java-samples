d95218c35b0ff953c9ecb924edaca4d4a60c57f9
powerLeePlus <wenqiang12312006@126.com>
2022-03-09 18:34:44 +0800

    java反射+1

--- java/basis/src/main/java/ReflectTest.java
+++ java/basis/src/main/java/ReflectTest.java
@@ -1,8 +1,11 @@
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+
+import javax.management.MXBean;
 
 /**
  * @author lwq
@@ -43,24 +46,38 @@ public class ReflectTest {
 		Class clazz = getClazz();
 		System.out.println(clazz.getName());
 
+		/**
+		 * getDeclared...   获取所有的属性、方法、构造器
+		 * get...           获取所有的public属性、方法、构造器
+		 */
 		// 获取 Person 类的所有方法信息
 		Method[] declaredMethods = clazz.getDeclaredMethods();
 		System.out.println(Arrays.toString(declaredMethods));
 		// 获取 Person 类的所有成员属性信息
 		Field[] declaredFields = clazz.getDeclaredFields();
 		System.out.println(Arrays.toString(declaredFields));
+		Field[] fields = clazz.getFields();
+		System.out.println(Arrays.toString(fields));
 		// 获取 Person 类的所有构造方法信息
 		Constructor[] declaredConstructors = clazz.getDeclaredConstructors();
 		System.out.println(Arrays.toString(declaredConstructors));
+		// 获取 Person 类的所有annotation信息
+		Annotation[] annotations = clazz.getAnnotations();
+		System.out.println(Arrays.toString(annotations));
 
 		// 通过反射创建对象
 		Person instance = getInstance(clazz);
 	}
 
+	@MXBean
 	public static class Person {
 		private String name;
 		private Integer gender;
 		private Integer age;
+
+		String addr;
+		protected String education;
+		public String nickname;
 
 		public Person() {
 		}
@@ -95,6 +112,30 @@ public class ReflectTest {
 			this.age = age;
 		}
 
+		public String getAddr() {
+			return addr;
+		}
+
+		public void setAddr(String addr) {
+			this.addr = addr;
+		}
+
+		public String getEducation() {
+			return education;
+		}
+
+		public void setEducation(String education) {
+			this.education = education;
+		}
+
+		public String getNickname() {
+			return nickname;
+		}
+
+		public void setNickname(String nickname) {
+			this.nickname = nickname;
+		}
+
 		@Override
 		public String toString() {
 			final StringBuilder sb = new StringBuilder("{");
@@ -104,6 +145,12 @@ public class ReflectTest {
 					.append(gender);
 			sb.append(",\"age\":")
 					.append(age);
+			sb.append(",\"addr\":\"")
+					.append(addr).append('\"');
+			sb.append(",\"education\":\"")
+					.append(education).append('\"');
+			sb.append(",\"nickname\":\"")
+					.append(nickname).append('\"');
 			sb.append('}');
 			return sb.toString();
 		}
