890336e09fb52fdf0911c9227f5e2e41699eaf68
powerLeePlus <wenqiang12312006@126.com>
2022-06-01 18:41:33 +0800

    String字符串存放与常量池

--- java/basis/src/main/java/StringTest.java
+++ java/basis/src/main/java/StringTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author lwq
  * @date 2022/3/2 0002
@@ -6,6 +9,13 @@
 public class StringTest {
 
 	public static void main(String[] args) {
+		testString();
+	}
+	
+	/**
+	 * 字符串常量、字符串对象
+	 */
+	public static void testString() {
 		/**
 		 * 问：String可以被继承吗
 		 * 答：String 类是 final 类，不可以被继承。
@@ -27,5 +37,38 @@ public class StringTest {
 		System.out.println(str2 == "hello"); //false
 		str2 = str1;
 		System.out.println(str2 == "hello"); //true
+
+		String s1 = "Hello";
+		String s2 = "Hello";
+		String s3 = "Hel" + "lo";
+		String s4 = "Hel" + new String("lo");
+		String s5 = new String("Hello");
+		String s6 = s5.intern(); // 尝试将s5值"Hello"放入常量池中，并返回在常量池中地址
+		String s7 = "H";
+		String s8 = "ello";
+		String s9 = s7 + s8;
+		System.out.println(s1 == s2); // true
+		System.out.println(s1 == s3); // true
+		System.out.println(s1 == s4); // false
+		System.out.println(s1 == s9); // false
+		System.out.println(s4 == s5); // false
+		System.out.println(s1 == s6); // true
+	}
+
+	/**
+	 * 测试字符串常量池不在堆上，在永久代（JDK1.8后元空间）中
+	 * 1、假设jvm启动参数为：-XX:PermSize＝2M -XX:MaxPermSize＝2M，JDK1.8：-XX:MaxMetaspaceSize=2M
+	 * 2、然后运行如下代码
+	 * 3、程序立刻会抛出：Exception in thread "main" java.lang.outOfMemoryError: PermGen space异
+	 * 常。PermGen space正是方法区，足以说明常量池在方法区中。
+	 *  在jdk8中，抛出：java.lang.OutOfMemoryError: Metaspace异常。同理说明运行时常量池是划分在Metaspace区域中。
+	 */
+	public static void testString2() {
+		List<String> list = new ArrayList<>();
+		int i = 0;
+		while (true) {
+			//通过intern方法向常量池中手动添加常量
+			list.add(String.valueOf(i++).intern());
+		}
 	}
 }
