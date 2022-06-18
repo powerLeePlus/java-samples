032d245be5ad03f83634aadaf82931e22c97b60b
powerLeePlus <wenqiang12312006@126.com>
2022-06-18 11:30:48 +0800

    ReentrantLock说明

--- java/multi-thread/src/main/java/com/lwq/example/multithread/lock/ReentrantLockDemo.java
+++ java/multi-thread/src/main/java/com/lwq/example/multithread/lock/ReentrantLockDemo.java
@@ -10,6 +10,9 @@ import java.util.concurrent.locks.ReentrantLock;
  * （2）synchronized可重入，因为加锁和解锁自动进行，不必担心最后是否释放锁；ReentrantLock也可重入，但加锁和解锁需要手动进行，且次数需一样，否则其他线程无法获得锁。
  * （3）synchronized不可响应中断，一个线程获取不到锁就一直等着；ReentrantLock可以响应中断。
  * （4）ReentrantLock还可以实现公平锁机制。什么叫公平锁呢？也就是在锁上等待时间最长的线程将获得锁的使用权。通俗的理解就是谁排队时间最长谁先执行获取锁。
+ *
+ * ReentrantLock+Condition 等效于 Synchronized+Object上的wait和notify/notifyAll，可以实现线程间等待通知机制
+ *
  * @author lwq
  * @date 2020/12/7 0007
  */
@@ -17,7 +20,26 @@ public class ReentrantLockDemo {
 
 	private static final ReentrantLock LOCK = new ReentrantLock();
 
+	/**
+	 * 最简单的锁
+	 */
 	private static void test1() {
+		LOCK.lock();
+		try {
+			System.out.println(Thread.currentThread().getName() + " 获得了锁");
+			TimeUnit.SECONDS.sleep(2L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			LOCK.unlock();
+			System.out.println(Thread.currentThread().getName() + " 释放了锁");
+		}
+	}
+
+	/**
+	 * ReentrantLock + Condition
+	 */
+	private static void test2() {
 		LOCK.lock();
 		try {
 			System.out.println(Thread.currentThread().getName() + " 获得了锁");
